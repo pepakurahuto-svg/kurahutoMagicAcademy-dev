408dddae3da17338b5d44756fb2aaf76d28f3c23
pepakurahuto-svg <pepakurahuto@gmail.com>
2026-09-14 14:44:38 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyAsndE02uekGOzOcY_WSvO5sJJ34CaQJdk",
+    authDomain: "kurahutomagicacademydev.firebaseapp.com",
+    projectId: "kurahutomagicacademydev",
+    storageBucket: "kurahutomagicacademydev.firebasestorage.app",
+    messagingSenderId: "258299314660",
+    appId: "1:258299314660:web:db304bcf1af87a0b3587f1"
+  };
