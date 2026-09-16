9ad182dfb925d8faf2cdc2e2f8ba62e7ca805dbe
pepakurahuto-svg <pepakurahuto@gmail.com>
2026-09-16 15:25:15 +0900

    Add files via upload

--- attributes.js
+++ attributes.js
@@ -1,6 +1,15 @@
+// 属性
 const MAGIC_ATTRIBUTES = {
   1: "火",
   2: "水",
   3: "風",
   4: "土"
+};
+
+// 属性カラー
+const MAGIC_ATTRIBUTE_COLORS = {
+  1: "#ffdddd", // 火：薄い赤
+  2: "#ddeeff", // 水：薄い青
+  3: "#ddffdd", // 風：薄い緑
+  4: "#f5e5cc"  // 土：薄い茶色
 };
