4185d3c9bcb65d3025b3eeab06c70d39202372e8
pepakurahuto-svg <pepakurahuto@gmail.com>
2026-09-19 13:32:49 +0900

    Add files via upload

--- attributes.js
+++ attributes.js
@@ -13,3 +13,19 @@ const MAGIC_ATTRIBUTE_COLORS = {
   3: "#ddffdd", // 風：薄い緑
   4: "#f5e5cc"  // 土：薄い茶色
 };
+
+// 属性チャンピオン
+const MAGIC_ATTRIBUTE_CHAMPION = {
+  1: "紅炎覇者", // 火
+  2: "蒼水覇者", // 水
+  3: "翠風覇者", // 風
+  4: "黄土覇者"  // 土
+};
+
+// 属性最大威力
+const MAGIC_ATTRIBUTE_MAXDAMAGE = {
+  1: "炎壊王", // 火
+  2: "水壊王", // 水
+  3: "風壊王", // 風
+  4: "土壊王"  // 土
+};
