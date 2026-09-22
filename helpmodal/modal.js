b9ff337a7380a8317f9be876aff172f9f86dc77f
pepakurahuto-svg <pepakurahuto@gmail.com>
2026-09-22 21:53:17 +0900

    Add files via upload

---
+++ helpmodal/modal.js
@@ -0,0 +1,15 @@
+async function openHelpModal() {
+  const overlay = document.getElementById("modalOverlay");
+  const content = document.getElementById("modalContent");
+
+  // help.html を読み込む
+  const res = await fetch("/help.html");
+  const html = await res.text();
+
+  content.innerHTML = html;
+  overlay.style.display = "flex";
+}
+
+function closeHelpModal() {
+  document.getElementById("modalOverlay").style.display = "none";
+}
